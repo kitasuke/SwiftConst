c6c4c95efdad1dd2a387ef44030ac9d211d5eeb4
kitasuke <yusuke2759@gmail.com>
2019-09-23 15:07:49 +0800

    Add unit tests for min length and ignore patterns

--- Tests/SwiftConstCoreTests/Visitors/StringVisitorTests.swift
+++ Tests/SwiftConstCoreTests/Visitors/StringVisitorTests.swift
@@ -31,9 +31,7 @@ struct A {
 }
 """
 
-        let url = createSourceFile(from: input)
-        let syntax = try! SyntaxParser.parse(url)
-        
+        let syntax = makeSyntax(from: input)
         let dataStore = MockDataStore()
         var visitor = makeStringVisitor(minimumLength: 1, ignorePatterns: [], syntax: syntax, with: dataStore)
         syntax.walk(&visitor)
@@ -41,11 +39,73 @@ struct A {
         XCTAssertEqual(
             dataStore.strings,
             [
-                makeSwiftConstString(value: "ddd", line: 2, column: 16),
-                makeSwiftConstString(value: "aaa", line: 6, column: 23),
-                makeSwiftConstString(value: "bbb", line: 7, column: 16),
+                makeString(value: "ddd", line: 2, column: 16),
+                makeString(value: "aaa", line: 6, column: 23),
+                makeString(value: "bbb", line: 7, column: 16),
             ]
         )
+    }
+    
+    func test_stringVisitor_minLength() {
+        let input = """
+struct A {
+    let foo = "foo"
+    let bar = "bar"
+
+    func foo() -> String {
+        let string = "aaaaa"
+        
+        print("foo")
+        print("bar")
+    }
+}
+"""
+
+        let syntax = makeSyntax(from: input)
+        let dataStore = MockDataStore()
+        var visitor = makeStringVisitor(minimumLength: 4, ignorePatterns: [], syntax: syntax, with: dataStore)
+        syntax.walk(&visitor)
+        
+        XCTAssertEqual(
+            dataStore.strings,
+            [
+                makeString(value: "aaaaa", line: 6, column:23),
+            ]
+        )
+    }
+    
+    func test_stringVisitor_minLength_ignorePatterns() {
+        let input = """
+struct A {
+    let foo = "foo"
+    let bar = "bar"
+
+    func foo() -> String {
+        let string = "aaaaa"
+        let number = "112233"
+        
+        print("foo")
+        print("bar")
+    }
+}
+"""
+
+        let syntax = makeSyntax(from: input)
+        let dataStore = MockDataStore()
+        var visitor = makeStringVisitor(minimumLength: 4, ignorePatterns: ["[0-9]+"], syntax: syntax, with: dataStore)
+        syntax.walk(&visitor)
+        
+        XCTAssertEqual(
+            dataStore.strings,
+            [
+                makeString(value: "aaaaa", line: 6, column:23),
+            ]
+        )
+    }
+    
+    private func makeSyntax(from input: String) -> SourceFileSyntax {
+        let url = createSourceFile(from: input)
+        return try! SyntaxParser.parse(url)
     }
     
     private func makeStringVisitor(minimumLength: Int, ignorePatterns: [String], syntax: SourceFileSyntax, with dataStore: MockDataStore) -> StringVisitor {
@@ -58,7 +118,7 @@ struct A {
         )
     }
     
-    private func makeSwiftConstString(value: String, line: Int, column: Int) -> SwiftConstString {
+    private func makeString(value: String, line: Int, column: Int) -> SwiftConstString {
         return .init(
             filePath: "",
             value: value,
